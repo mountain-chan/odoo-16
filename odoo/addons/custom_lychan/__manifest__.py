f2e7d07a31e448ebe87de8121b9d557c9bc2f10f
lychan <chan.ly@advesa.com>
2023-11-10 17:01:11 +0700

    add custom addons

---
+++ odoo/addons/custom_lychan/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Ly Chan Custom',
+    'version': '1.0',
+    'summary': 'Ly Chan Custom summary',
+    'sequence': 10,
+    'description': """
+Ly Chan Custom description
+    """,
+    'category': 'Accounting/Accounting',
+    'website': 'https://mountainchan575980059.wordpress.com/',
+    'depends' : ['base_setup', 'product', 'analytic', 'portal', 'digest'],
+    'license': 'LGPL-3',
+}
